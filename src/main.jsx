c740515a804d60fdf57828c70d114929ff2998a1
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-29 19:21:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-function SafeApp() {
-  try {
-    return <App />;
-  } catch (e) {
-    return (
-      <div style={{ padding: 20, color: 'red' }}>
-        <h2>App crashed on load</h2>
-        <pre>{String(e)}</pre>
-      </div>
-    );
-  }
-}
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SafeApp />
+    <App />
   </React.StrictMode>
 );
