4a6847cefd4c5c0931f872ae101e84bc6b923cf1
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-29 15:11:31 +0530

    Update menu.api.js

--- src/api/menu.api.js
+++ src/api/menu.api.js
@@ -1,1 +1,12 @@
+import { supabase } from './supabaseClient';
+
+export async function getMenu() {
+  const { data, error } = await supabase
+    .from('menu_items')
+    .select('*')
+    .eq('is_active', true);
+
+  if (error) throw error;
+  return data;
+}
 
