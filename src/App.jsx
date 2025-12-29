f88140d8cb24aad5f7bdba187ebb0986cfe756cb
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-29 15:46:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Checkout from './pages/Checkout';
 
 export default function App() {
   return (
-    <>
+    <div style={{ padding: '16px', maxWidth: '480px', margin: '0 auto' }}>
+      <h1>Pani Puri Delivery</h1>
       <Home />
       <Checkout />
-    </>
+    </div>
   );
 }
-
