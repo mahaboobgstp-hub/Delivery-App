c46d0d5b3dfe74d261ebc65db9c8873ad4b1c8cd
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-29 15:12:14 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,22 @@
+import { useEffect, useState } from 'react';
+import { getMenu } from '../api/menu.api';
+
+export default function Home() {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    getMenu().then(setMenu);
+  }, []);
+
+  return (
+    <div>
+      <h2>Pani Puri Family Pack</h2>
+      {menu.map(item => (
+        <div key={item.id}>
+          <p>{item.name} - ₹{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
