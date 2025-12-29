cba76a556eec5bb5f2725f446330fafde4ecfca2
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-29 15:13:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import Home from './pages/Home';
+import Checkout from './pages/Checkout';
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <Checkout />
+    </>
+  );
+}
 
